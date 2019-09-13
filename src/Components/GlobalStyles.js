8d9c57cee6a51b72fbb46c71595daa86666856e8
soromi <soromii@naver.com>
2019-09-14 02:26:06 +0900

    styled GlobalStyles, Header

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -3,6 +3,22 @@ import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
     ${reset};
+    html, body {
+        width: 100%;
+        height: 100%;
+        background: black;
+    }
+    body {
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size:12px;
+        background-color:rgba(20, 20, 20, 1);
+        color:white;
+        padding-top:50px; 
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
 `;
 
 export default globalStyles;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header`
+	display: flex;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 50px;
+	align-items: center;
+	color: white;
+	background-color: rgba(20, 20, 20, 0.8);
+	z-index: 10;
+	box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+	display: flex;
+`;
+
+const Item = styled.li`
+	width: 80px;
+	height: 50px;
+	text-align: center;
+`;
+
+const ALink = styled(Link)`
+	display: flex;
+	height: 50px;
+	align-items: center;
+	justify-content: center;
+`;
 
 export default () => (
-	<header>
-		<ul>
-			<li>
-				<Link to="/">Movie</Link>
-			</li>
-			<li>
-				<Link to="/tv">TV</Link>
-			</li>
-			<li>
-				<Link to="/search">Search</Link>
-			</li>
-		</ul>
-	</header>
+	<Header>
+		<List>
+			<Item>
+				<ALink to="/">Movie</ALink>
+			</Item>
+			<Item>
+				<ALink to="/tv">TV</ALink>
+			</Item>
+			<Item>
+				<ALink to="/search">Search</ALink>
+			</Item>
+		</List>
+	</Header>
 );
