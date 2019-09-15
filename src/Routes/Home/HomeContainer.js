8b14732d8d936d17eb547e30bc81d61f66d3ecc6
soromi <soromii@naver.com>
2019-09-16 03:20:18 +0900

    update TVContainer, HomeContainer (connect api)

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
 	state = {
@@ -9,6 +10,25 @@ export default class extends React.Component {
 		loading: true,
 		error: null
 	};
+
+	async componentDidMount() {
+		try {
+			const {
+				data: { results: nowPlaying }
+			} = await moviesApi.nowPlaying();
+			const {
+				data: { results: upcoming }
+			} = await moviesApi.upcoming();
+			const {
+				data: { results: popular }
+			} = await moviesApi.popular();
+			this.setState({ nowPlaying, upcoming, popular });
+		} catch (error) {
+			this.setState({ error: "Can't find movies information" });
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
 
 	render() {
 		const { nowPlaying, upcoming, popular, loading, error } = this.state;
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 export default class extends React.Component {
 	state = {
@@ -9,6 +10,25 @@ export default class extends React.Component {
 		loading: true,
 		error: null
 	};
+
+	async componentDidMount() {
+		try {
+			const {
+				data: { results: airingToday }
+			} = await tvApi.airingToday();
+			const {
+				data: { results: topRated }
+			} = await tvApi.topRated();
+			const {
+				data: { results: popular }
+			} = await tvApi.popular();
+			this.setState({ airingToday, topRated, popular });
+		} catch (error) {
+			this.setState({ error: "Can't find movies information" });
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
 
 	render() {
 		const { airingToday, topRated, popular, loading, error } = this.state;
