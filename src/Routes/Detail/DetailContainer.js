d79add9983197dcd292570f0aed1d3248e0d19e5
soromi <soromii@naver.com>
2019-09-16 03:24:41 +0900

    set detail at Router / update DetailContainer (connect api)

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,53 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
-	state = {
-		result: null,
-		loading: true,
-		error: null
-	};
+	constructor(props) {
+		super(props);
+		const {
+			location: { pathname }
+		} = props;
+		this.state = {
+			result: null,
+			loading: true,
+			error: null,
+			isMovie: pathname.includes("/movie/")
+		};
+	}
+
+	async componentDidMount() {
+		const {
+			match: {
+				params: { id }
+			},
+			history: { push }
+		} = this.props;
+
+		// id가 숫자로만 이루어져 있는지 체크
+		// 문자가 섞여있으면 기본 url로 리다이렉트
+		if (id.match(/^[0-9]+$/) === null) {
+			return push("/");
+		}
+
+		let result = null;
+		try {
+			if (this.isMovie) {
+				({ data: result } = await moviesApi.detail(id));
+			} else {
+				({ data: result } = await tvApi.detail(id));
+			}
+		} catch (error) {
+			this.setState({
+				erroe: "Can't find anything"
+			});
+		} finally {
+			this.setState({
+				loading: false,
+				result
+			});
+		}
+	}
 
 	render() {
 		const { result, loading, error } = this.state;
