4b4257887300aa8388f0e795d706f292c9f6051b
soromi <soromii@naver.com>
2019-09-17 01:17:35 +0900

    set propTypes at each Presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,16 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({ airingToday, topRated, popular, loading, error }) =>
+	null;
+
+TVPresenter.propTypes = {
+	airingToday: PropTypes.array,
+	topRated: PropTypes.array,
+	popular: PropTypes.array,
+	loading: PropTypes.bool.isRequired,
+	error: PropTypes.string
+};
+
+export default TVPresenter;
