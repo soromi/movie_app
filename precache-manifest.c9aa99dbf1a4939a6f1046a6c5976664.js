1ac73185dc520af8ebea698c8a99abca1103aa25
soromi <soromii@naver.com>
2019-09-19 14:08:12 +0900

    Updates

---
+++ precache-manifest.c9aa99dbf1a4939a6f1046a6c5976664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a87a2481169b72dc8911ef0b70ff08",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3ce260d8166be99fa2ea",
+    "url": "/movie_app/static/js/2.909dc61b.chunk.js"
+  },
+  {
+    "revision": "30e013df3271a3a5ca42",
+    "url": "/movie_app/static/js/main.065d6012.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  },
+  {
+    "revision": "325c546d2e26969ab54f03147e2bb975",
+    "url": "/movie_app/static/media/icon_imdb.325c546d.png"
+  }
+]);
